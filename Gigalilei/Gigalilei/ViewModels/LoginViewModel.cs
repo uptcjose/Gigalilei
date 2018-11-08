1b41e17ba10ec0b15b41a4821a6ce7d8f7f19663
uptcjose <44174832+uptcjose@users.noreply.github.com>
2018-11-08 17:00:01 -0500

    baseviemodel

--- Gigalilei/Gigalilei/ViewModels/LoginViewModel.cs
+++ Gigalilei/Gigalilei/ViewModels/LoginViewModel.cs
@@ -4,9 +4,20 @@
 
 namespace Gigalilei.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModel
     {
+        
+
+        #region atributes
+        private string User;
+        private string Password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
         #region properties
         public string user
         {
@@ -15,24 +26,98 @@ namespace Gigalilei.ViewModels
         }
         public string password
         {
-            get;
-            set;
+            get {return this.Password;}
+            set{ SetValue(ref Password, value); }
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref isRunning, value); }
         }
         public bool IsRemember
         {
             get;
             set;
         }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref isEnabled, value); }
+        }
         #region command
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+
+        }
+
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.user))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "Por favor Introduzca un usuario",
+                    "Aceptar");
+                return;
+
+                this.IsRunning = true;
+                this.isEnabled = false;
+            }
+            
+            if (string.IsNullOrEmpty(this.password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "Por favor Introduzca su contraseña",
+                    "Aceptar");
+                return;
+                this.IsRunning = false;
+                this.isEnabled = false;
+            }
+            
+
+            if (this.user != "12")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "Usuario incorrecto",
+                    "Aceptar");
+                this.user = string.Empty;
+                return;
+                this.IsRunning = false;
+                this.isEnabled = false;
+            }
+            if (this.password != "12")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "error",
+                    "Contraseña incorrecta",
+                    "Aceptar");
+                this.password = string.Empty;
+                this.IsRunning = false;
+                this.isEnabled = false;
+            }
+          
+            if (this.user == "12")
+            {
+                if (this.password == "12")
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                       "Bien",
+                       "pagina siguiente",
+                       "Aceptar");
+                    this.IsRunning = false;
+                    this.isEnabled = true;
+                }
+                return;
+            }
+            
         }
         #endregion
         #endregion
@@ -41,6 +126,7 @@ namespace Gigalilei.ViewModels
         public LoginViewModel()
         {
             this.IsRemember = true;
+            this.isEnabled = true;
         }
         #endregion
     }
